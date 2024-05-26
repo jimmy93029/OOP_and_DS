3ad0e77f94f97c089918f815544475692b111a50
jimmywu <jimmywu0229@gmail.com>
2024-05-26 16:19:44 +0800

    hw4 init

---
+++ hw3/OOP_HW03_Template_NET2019_202402/00_StudentWork/00_SimpleInteractiveSystem/graph_basics.h
@@ -0,0 +1,43 @@
+//
+//
+// Prof. Sai-Keung Wong
+// Email:	cswingo@nycu.edu.tw
+//
+// National Yang Ming Chiao Tung University, Taiwan
+// Computer Science
+// Date: 2024/05/01
+//
+//
+
+#ifndef __MY_GRAPH_BASICS_H_
+#define __MY_GRAPH_BASICS_H_
+#include "../headers.h"
+#include <string>
+#include "../primitives/vector3d.h"
+#include <vector>
+using std::vector;
+
+class GRAPH_NODE {
+public:
+    GRAPH_NODE( ) {
+        r = 1.0;
+        p = vector3(0.0, 0.0, 0.0);
+    }
+    vector3 p;  //position
+    double r;   //radius    
+
+    int id;     //unique ID
+    int dynamicID; // in the active index array
+
+    vector<int> edgeID; //incident
+    //
+};
+
+class GRAPH_EDGE {
+public:
+    int id;
+    int dynamicID; 
+    int nodeID[2]; //0, 1
+};
+
+#endif
